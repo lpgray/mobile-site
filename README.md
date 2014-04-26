0cbf4304661f37c689bed155eae3fd7c5b3b10ca
lpgray <rayzy1991@gmail.com>
2014-04-26 18:45:32 +0800

    add new page.

--- README.md
+++ README.md
@@ -29,8 +29,18 @@ Offical Mobile Site Static Pages
 - Carousel(jqmobile or not)  DONE
 - HOME Page(no theme) DONE
 - theme restructure
-	- put jqmobile and ms together
-- moke data
+	- put jqmobile and ms together DONE
+- Page
+	- home DONE
+	- about DONE
+	- feed DONE
+	- article DONE
+	- list DONE
+	- welcome to use DONE
+	- gallery 
 - widgets dev
 	- image preview
 - icons & colors design
+	- win8 style
+	- normal
+- moke data and mvc
--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,24 @@
+$.mobile.defaultPageTransition = 'slide';
+
 $(document).ready(function(){
+	//welcome carousel
+	$('#J_welcomecarousel').find('.item').height($(window).height());
+	$('#J_welcomecarousel').owlCarousel({
+		items : 3,
+		navigation : false,
+		singleItem : true
+	});
+
+	//carousel init
 	$('#J_owl').owlCarousel({
-		navigation : 0,
+		navigation : false,
 		slideSpeed : 300,
 		pageinationSpeed : 3000,
 		singleItem : true,
 		autoPlay : true
 	});
+
+	//baidu map init
+	var map = new BMap.Map("J_mapctn");  
+	map.centerAndZoom(new BMap.Point(116.404, 39.915), 14);
 });
